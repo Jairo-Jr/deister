b069ca317234a6a1c4ebe0cfab3fc2660684f3c6
Jairo Huallpa <paul.rojas@Jairo-Huallpa.local>
2023-01-12 10:57:23 -0500

    Correccion del script, uso de comentarios y funciones para mejor entendido

--- load_file/crp_load_rrhh_file_v1.js
+++ load_file/crp_load_rrhh_file_v1.js
@@ -26,8 +26,8 @@
  * 
  *  JS:  crp_load_rrhh_file 
  *
- *  Version     : v2.0
- *  Date        : 2023-01-11
+ *  Version     : v2.1
+ *  Date        : 2023-01-12
  *  Description : Procesa ficheros .xls según el tipo de proceso (Costo/Planilla); 
  *                registra los Costos en la tabla de respaldo (crp_rrhh_asign) 
  *                y las planillas en los Mov. Contables (capuntes).
@@ -179,7 +179,7 @@
     } 
 
     /**
-     * LOCAL FUNCTION: __validateGroupAux
+     * LOCAL FUNCTION: __insCodEmp
      * 
      * Description: Función local que agrega los códigos de empleados al 
      *              auxiliar asociado a cuentas(t: cctaauxl)
@@ -246,8 +246,15 @@
     } 
 
     /**
+     * LOCAL FUNCTION: __updFileStatus
      * 
-     * @param {*} pStrStatus 
+     * Description: Función local que actualiza el estado del fichero.
+     * 
+     * PARAMETERS: 
+     *      @param {string}     pStrStatus      Estado del fichero (P: Pendiente, C: Cargado, E: Error)
+     *      @param {integer}    pIntLoteId      Identificador de lote
+     *      @param {string}     pStrUserName    Usuario que realiza el proceso
+     *      @param {integer}    pIntFileId      Identificador del fichero
      */
     function __updFileStatus(pStrStatus, pIntLoteId, pStrUserName, pIntFileId) { 
         /**
@@ -331,22 +338,6 @@
         switch (pStrProc) {
             // Proceso Centro de Costos
             case 'C': 
-                // mRsSheet.forEach(mRowSheet => { 
-                //     /**
-                //      * Insert en la tabla de respaldo
-                //      */
-                //     Ax.db.insert("crp_rrhh_asign", { 
-                //         file_seqno: pIntFileId,
-                //         dcodcos : mRowSheet.A,
-                //         dctagas : mRowSheet.B,
-                //         dctacos : mRowSheet.C,
-                //         dvalcom : mRowSheet.D,
-                //         tip_proc: pStrTipProc,
-                //         user_created: mStrUserName,
-                //         user_updated: mStrUserName
-                //     });
-    
-                // });
                 /**
                  * Insert de costos
                  */
@@ -362,145 +353,13 @@
                 /**
                  * Validar la existencia del grupo auxiliar de RRHH.
                  */
-                // var mIntExistGroupAux = Ax.db.executeGet(`
-                //     <select>
-                //         <columns>
-                //             COUNT(*)
-                //         </columns>
-                //         <from table='cctaauxh'/>
-                //         <where>
-                //             cctaauxh.codaux = ?
-                //         </where>
-                //     </select> 
-                // `, 'RRHH'); 
-
-                // if (mIntExistGroupAux != 1) {
-                //     /**
-                //      * Insert del grupo auxiliar debido a su inexistencia.
-                //      */
-                //     Ax.db.insert("cctaauxh", {
-                //         codaux: 'RRHH',
-                //         desaux: 'RRHH'
-                //     }); 
-                // }
                 __validateGroupAux();
 
-                /** */
-                // /**
-                //  * Se obtiene el identificador de lote 'loteid'.
-                //  */
-                // mIntLoteId = Ax.db.executeGet(`
-                //     <select>
-                //         <columns>
-                //             MAX(loteid) max_lote_id
-                //         </columns>
-                //         <from table='capuntes'/>
-                //     </select> 
-                // `) + 1; 
-
-                // /**
-                //  * Registro en Movimientos contables 'capuntes'.
-                //  */
-                // mRsSheet.forEach(mRowSheet => { 
-                //     /**
-                //      * Componer el número de documento según la nulidad de la serie y el comprobante.
-                //      */
-                //     var mStrDocSer = (mRowSheet.I === null || mRowSheet.J === null) ? '-' : mRowSheet.I + '-' + mRowSheet.J;
-
-                //     // Registro de planilla en 'capuntes'
-                //     Ax.db.insert("capuntes", {
-                //         empcode: '001', 
-                //         proyec: 'CRP0',
-                //         sistem: 'A', 
-                //         seccio: '0',
-                //         fecha: mRowSheet.C,
-                //         diario: '40', 
-                //         jusser: 'GL',
-                //         origen: 'M',
-                //         docser: mStrDocSer,
-                //         punteo: 'N',
-                //         placon: 'CH',
-                //         cuenta: mRowSheet.E,
-                //         codaux: 'RRHH',
-                //         ctaaux: mRowSheet.F,
-                //         contra: null,
-                //         concep: mRowSheet.G,
-                //         fecval: mRowSheet.K,
-                //         moneda: 'PEN',
-                //         divdeb: mRowSheet.M,
-                //         divhab: mRowSheet.N,
-                //         cambio: '1.000000',
-                //         divemp: 'PEN',
-                //         debe: mRowSheet.M,
-                //         haber: mRowSheet.N,
-                //         loteid: mIntLoteId,
-                //         user_created: mStrUserName,
-                //         user_updated: mStrUserName
-                //     });
-                // });
                 /**
                  * Insert de planillas
                  */
                 __insPlanilla(mRsSheet, mStrUserName);
 
-                // /**
-                //  * Agrupado de los códigos de empleado.
-                //  */ 
-                // var mRsCtaAux = Ax.db.executeQuery(` 
-                //     <select>
-                //         <columns>
-                //             DISTINCT capuntes.ctaaux
-                //         </columns>
-                //         <from table='capuntes'/>
-                //         <where>
-                //         capuntes.loteid = ?;
-                //         </where>
-                //     </select>
-                // `, mIntLoteId);
-
-
-                // /**
-                //  * Recorrido de los códigos de empleado.
-                //  */
-                // mRsCtaAux.forEach(mIntCtaAux => { 
-
-                //     /**
-                //      * Si no es null
-                //      */
-                //     if (mIntCtaAux.ctaaux !== null) { 
-                //         /**
-                //          * Búsqueda si se encuentra ya registrado el código de empleado.
-                //          */
-                //         var mIntExistCtaAux = Ax.db.executeGet(` 
-                //             <select>
-                //                 <columns>
-                //                     COUNT(*)
-                //                 </columns>
-                //                 <from table='cctaauxl'/>
-                //                 <where>
-                //                     cctaauxl.ctaaux = ?;
-                //                 </where>
-                //             </select>
-                //         `, mIntCtaAux.ctaaux); 
-
-                //         /**
-                //          * Si la cantidad de registro es menor/igual a cero (No está registrado).
-                //          */
-                //         if (mIntExistCtaAux <= 0) {
-                //             /**
-                //              * Insert del código de empleado en el registro auxiliar (cctaauxl).
-                //              */
-                //             Ax.db.insert("cctaauxl", {
-                //                 codaux: 'RRHH',
-                //                 ctaaux: mIntCtaAux.ctaaux,
-                //                 desval: mIntCtaAux.ctaaux,
-                //                 estado: 'A',
-                //                 user_created: mStrUserName,
-                //                 user_updated: mStrUserName
-                //             });
-                //         } 
-                //     } 
-                // }); 
                 /** 
                  * Registro de los códigos de empleados.
                  */
@@ -517,41 +376,12 @@
                 throw new Ax.ext.Exception('El tipo de proceso es no soportado.');
         } 
 
-        // /**
-        //  * Update de estado de fichero.
-        //  */
-        // Ax.db.update("crp_rrhh_file", 
-        // 	{
-	    //         file_status  : 'C',
-        //         loteid: mIntLoteId,
-	    //         user_updated : mStrUserName,
-	    //         date_updated : new Ax.util.Date()
-	    // 	},
-	    // 	{
-	    // 		file_seqno : pIntFileId
-	    // 	}
-	    // );
-
         Ax.db.commitWork();
 
     } catch(error) { 
         console.error(error);
 
         Ax.db.rollbackWork(); 
-
-        /**
-         * Update de estado de fichero.
-         */
-        // Ax.db.update("crp_rrhh_file", 
-        // 	{
-	    //         file_status  : 'E',
-	    //         user_updated : mStrUserName,
-	    //         date_updated : new Ax.util.Date()
-	    // 	},
-	    // 	{
-	    // 		file_seqno : pIntFileId
-	    // 	}
-	    // );
         /**
          * Update del estado del fichero a Cargado (C)
          */
