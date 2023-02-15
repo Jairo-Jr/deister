2201331594516bc8cd6ea7242809119bceec9f33
Jairo Huallpa <paul.rojas@Jairo-Huallpa.local>
2023-02-15 08:28:50 -0500

    Se termino API setreceivedpurchase

--- API/setreceivedpurchase.js
+++ API/setreceivedpurchase.js
@@ -1,51 +1,60 @@
-function main(data) { 
+function main(data) {
 
     try {
         var mStrJsonData = JSON.stringify(data);
         // Registro de data de Orden de Compra
         var mIntSerial = Ax.db.insert('crp_ibth_setreceivedpurchase_h', {
-            purchasecode: data.code,
-            referralguide: data.gr,
-            version: data.version,
-            json_receivedpurchase: mStrJsonData
+            purchasecode:           data.code,
+            referralguide:          data.gr,
+            version:                data.version,
+            json_receivedpurchase:  mStrJsonData
         }).getSerial();
 
 
         data.purchaseitem.forEach(item => {
             // Registro de productos
             Ax.db.insert('crp_ibth_setreceivedpurchase_l', {
-                id_receivedpurchase_h: mIntSerial,
-                numline: item.numline,
-                productcode: item.code,
-                brand: item.variante.brand,
-                size: item.variante.size,
-                lab: item.variante.lab,
-                qty: item.qty,
-                serial: item.serial,
-                batchcode: item.batch.code,
-                expdate: item.batch.expdate,
-                warehouse: item.warehouse
+                id_receivedpurchase_h:  mIntSerial,
+                numline:                item.numline,
+                productcode:            item.code,
+                brand:                  item.variante.brand,
+                size:                   item.variante.size,
+                lab:                    item.variante.lab,
+                qty:                    item.qty,
+                serial:                 item.serial,
+                batchcode:              item.batch.code,
+                expdate:                item.batch.expdate,
+                warehouse:              item.warehouse
             });
         });
 
-    } catch (error) { 
+    } catch (error) {
         return new Ax.net.HttpResponseBuilder()
-        .status(400)
-        .entity({
-            "status": "ERROR",
-            "message": error
-        })
-        .type("application/json")
-        .build();
+            .status(400)
+            .entity({
+                "response": {
+                    "status": "ERROR",
+                    "message": error
+                }
+            })
+            .type("application/json")
+            .build();
     }
 
 
     return new Ax.net.HttpResponseBuilder()
         .status(201)
         .entity({
-            "status": "OK",
-            "message": "Registro realizado"
+            "response": {
+                "status": "OK",
+                "message": "Registro realizado"
+            }
         })
         .type("application/json")
         .build();
 }
+
+
+// /lista/precio/producto
+// /guia-interna
+// /guia-interna-v1
