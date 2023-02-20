d22e00dd4712e7463ac1f546c4ed7641857fa8bb
Jairo Huallpa <paul.rojas@Jairo-Huallpa.local>
2023-02-20 15:05:17 -0500

    Fin de validaciones para albaranes DGCO - RGCO

---
+++ Validaciones/consumo_albaran.js
@@ -0,0 +1,39 @@
+/** 
+ * 
+ */
+ class w26235 extends workflowProg {
+    constructor() {
+        super("w26235", ["refter"]);
+    }
+    
+    
+    run(data) {
+        // Número de líneas con consumo total o parcial.
+        var mIntCount = Ax.db.executeGet(`
+                <select>
+                    <columns>
+                        COUNT(*)
+                    </columns>
+                    <from table='gcomalbh'>
+                        <join table='gcomalbl'>
+                            <on>gcomalbh.cabid = gcomalbl.cabid</on>
+                        </join>
+                    </from>
+                    <where>
+                            gcomalbh.refter = ?
+                        AND gcomalbl.auxchr2 IN ('P', 'S')
+                        AND  gcomalbh.tipdoc = 'AGCO'
+                    </where>
+                </select>
+            `, data.refter);
+        
+        return (mIntCount > 0 ) ? true : false
+        
+    }
+}
+
+
+/**
+ * ADICIONAL: 
+ *  - gcommovh: es la tabla de validacion debido a que no existe una tabla fisica gcomalbh
+ */
