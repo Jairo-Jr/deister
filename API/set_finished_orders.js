f9a29e9430936549f2cfdb5454aa0b5f1dd58877
Jairo Huallpa <paul.rojas@Jairo-Huallpa.local>
2023-02-27 17:53:15 -0500

    Ordenado del API finished_orders

--- API/set_finished_orders.js
+++ API/set_finished_orders.js
@@ -1,40 +1,38 @@
 function main(data) {
 
     try {
-        // Transformación a string el JSON recibido.
-        var mStrJsonData = JSON.stringify(data);
 
+        // ===============================================================
         // Iteracion de órdenes de pedidos finalizados (outputorders)
-        data.outputorders.forEach(mObjOrderH => {
-            // mObjOrderH.json_finishedorders = mStrJsonData;
-            // Registro de la data de cabecera
-            var mIntSerialOrderH = Ax.db.insert('crp_ibth_setfinishedorders_h', {
-                orderclient: mObjOrderH.OrderClient,
-                orderibt: mObjOrderH.OrderIBT,
-                ordertype: mObjOrderH.OrderType,
-                version: mObjOrderH.Version,
-                json_finishedorders: mStrJsonData
-            }).getSerial();
+        // ===============================================================
+        data.OutputOrders.forEach(mObjOrderH => {
+            // ===============================================================
+            // Transformación a string el JSON recibido.
+            // ===============================================================
+            var mStrJsonData = JSON.stringify(mObjOrderH);
+            mObjOrderH.json_receivedpurchase = mStrJsonData;
 
             var mIntSerialOrderH = Ax.db.insert('crp_ibth_setfinishedorders_h', mObjOrderH).getSerial();
 
+            // ===============================================================
             // Iteracion de líneas del pedido (orderitem)
+            // ===============================================================
             mObjOrderH.OrderItem.forEach(mObjOrderL => {
 
                 // Registro de la data de líneas
                 Ax.db.insert('crp_ibth_setfinishedorders_l', {
-                    id_finishedorders_h: mIntSerialOrderH,
-                    numline: mObjOrderL.NumLine,
-                    containercode: mObjOrderL.ContainerCode,
-                    qtyrequested: mObjOrderL.QtyRequested,
-                    qtyserved: mObjOrderL.QtyServed,
-                    productcode: mObjOrderL.Code,
-                    brand: mObjOrderL.Variante.Brand,
-                    size: mObjOrderL.Variante.Size,
-                    lab: mObjOrderL.Variante.Lab,
-                    serial: mObjOrderL.Serial,
-                    batchcode: mObjOrderL.Batch.Code,
-                    expdate: mObjOrderL.Batch.ExpDate
+                    id_finishedorders_h:    mIntSerialOrderH,
+                    numline:                mObjOrderL.NumLine,
+                    containercode:          mObjOrderL.ContainerCode,
+                    qtyrequested:           mObjOrderL.QtyRequested,
+                    qtyserved:              mObjOrderL.QtyServed,
+                    productcode:            mObjOrderL.Code,
+                    brand:                  mObjOrderL.Variante.Brand,
+                    size:                   mObjOrderL.Variante.Size,
+                    lab:                    mObjOrderL.Variante.Lab,
+                    serial:                 mObjOrderL.Serial,
+                    batchcode:              mObjOrderL.Batch.Code,
+                    expdate:                mObjOrderL.Batch.ExpDate
                 });
             });
         });
