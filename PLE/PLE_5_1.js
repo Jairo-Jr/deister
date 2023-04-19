d8e990f94e43288b517e8e5d9edf6f0b86f07068
Jairo Huallpa <paul.rojas@Jairo-Huallpa.local>
2023-04-19 18:26:11 -0500

    Desarrollod de script PLE 5-1, finalizado a espera de confirmacion

--- PLE/PLE_5_1.js
+++ PLE/PLE_5_1.js
@@ -23,27 +23,13 @@ var mDateTimeFinPeriodoInf = new Ax.util.Date(mIntYear, mIntMonth + 1, 0);
 var mStrCodPeriodo = mIntYear.toString() + (mIntMonth < 10 ? '0'+mIntMonth : mIntMonth) + '00';
 
 // ===============================================================
-// Definición del periodo actual
-// ===============================================================
-var mDateToday  = new Ax.util.Date();
-var mYearToday  = mDateToday.getYear();
-var mMonthToday = mDateToday.getMonth() + 1;
-
-// ===============================================================
-// Construcción de la primera y ultima fecha del mes, 
-// correspondiente al periodo actual.
-// ===============================================================
-var mDateTimeIniPeriodoAct = new Ax.util.Date(mYearToday, mMonthToday, 1);
-var mDateTimeFinPeriodoAct = new Ax.util.Date(mYearToday, mMonthToday + 1, 0); 
-
-// ===============================================================
 // TABLA TEMPORAL PARA CARTERA DE EFECTOS
 // ===============================================================
 let mTmpTableCefectos = Ax.db.getTempTableName(`@tmp_tbl_cefectos`);
 Ax.db.execute(`DROP TABLE IF EXISTS ${mTmpTableCefectos}`);
 
 Ax.db.execute(`
-    <select intotemp='@tmp_tbl_cefectos'>º
+    <select intotemp='${mTmpTableCefectos}'>
         <columns>
             capuntes.apteid,
             MAX(cefectos.fecven) fecven
@@ -75,55 +61,103 @@ Ax.db.execute(`
 var mRsPle_5_1 = Ax.db.executeQuery(`
     <select>
         <columns>
-            CAST('${mStrCodPeriodo}' AS VARCHAR(8))         <alias name='campo1' />,
-            CAST('CUO20230400' AS VARCHAR(40))              <alias name='campo2' />,
-            CAST('MCUO20230400' AS VARCHAR(10))             <alias name='campo3' />,
-            CAST(capuntes.cuenta AS VARCHAR(24))            <alias name='campo4' />,
-            CAST(capuntes.proyec AS VARCHAR(24))            <alias name='campo5' />,
-            CAST(capuntes.seccio AS VARCHAR(24))            <alias name='campo6' />,
-            CAST(capuntes.moneda AS VARCHAR(3))             <alias name='campo7' />,
-            CAST('' AS VARCHAR(1))                          <alias name='campo8' />,
-            CAST(ctax_move_head.taxh_cifter AS VARCHAR(15)) <alias name='campo9' />,
-            CAST(ctax_move_head.taxh_auxchr4 AS VARCHAR(2)) <alias name='campo10' />,
+            capuntes.apteid,
+            CAST('${mStrCodPeriodo}' AS VARCHAR(8))                 <alias name='campo1' />,
+            CAST('CUO'||capuntes.apteid AS VARCHAR(40))             <alias name='campo2' />,
+            CAST('MCUO${mStrCodPeriodo}' AS VARCHAR(10))            <alias name='campo3' />,
+
+            CAST(
+                CASE WHEN CHARINDEX('.', capuntes.cuenta) &gt; 0
+                        THEN SUBSTR(capuntes.cuenta, 1, CHARINDEX('.', capuntes.cuenta)-1) || SUBSTR(capuntes.cuenta, CHARINDEX('.', capuntes.cuenta)+1)
+                    ELSE capuntes.cuenta
+                END
+                AS VARCHAR(24)
+            )                                                       <alias name='campo4'/>,
+
+            CAST(capuntes.proyec AS VARCHAR(24))                    <alias name='campo5' />,
+            CAST(capuntes.seccio AS VARCHAR(24))                    <alias name='campo6' />,
+            CAST(capuntes.moneda AS VARCHAR(3))                     <alias name='campo7' />,
+            CAST('' AS VARCHAR(1))                                  <alias name='campo8' />,
+            CAST(ctax_move_head.taxh_cifter AS VARCHAR(15))         <alias name='campo9' />,
+            CAST(ctax_move_head.taxh_auxchr4 AS VARCHAR(2))         <alias name='campo10' />,
             CAST(
                 SUBSTR(ctax_move_head.taxh_refter, 1, CHARINDEX('-', ctax_move_head.taxh_refter)-1)
                 AS VARCHAR(20)
-            )                                               <alias name='campo11' />,
+            )                                                       <alias name='campo11' />,
+
             CAST(
                 SUBSTR(ctax_move_head.taxh_refter, CHARINDEX('-', ctax_move_head.taxh_refter)+1)
                 AS VARCHAR(20)
-            )                                               <alias name='campo12' />,
-            TO_CHAR(capuntes.fecha, '%d/%m/%Y')             <alias name='campo13' />,
-            TO_CHAR(tmp_cefectos.fecven, '%d/%m/%Y')        <alias name='campo14' />,
-            TO_CHAR(ctax_move_head.taxh_fecdoc, '%d/%m/%Y') <alias name='campo15' />,
-            CAST(capuntes.concep AS VARCHAR(200))           <alias name='campo16' />,
-            ''                                              <alias name='campo17' />,
-            CAST(ROUND(capuntes.debe, 2) AS VARCHAR(15))    <alias name='campo18' />,
-            CAST(ROUND(capuntes.haber, 2) AS VARCHAR(15))   <alias name='campo19' />,
-
-            ''                                              <alias name='campo20' />,
-            CASE WHEN capuntes.fecha BETWEEN ? AND ?
-                    THEN '1'
-                WHEN capuntes.fecha &lt; ?
-                    THEN '8'
-                ELSE '9'
-            END                                             <alias name='campo21' />,
+            )                                                       <alias name='campo12' />,
+            TO_CHAR(capuntes.fecha, '%d/%m/%Y')                     <alias name='campo13' />,
+            TO_CHAR(tmp_cefectos.fecven, '%d/%m/%Y')                <alias name='campo14' />,
+            TO_CHAR(ctax_move_head.taxh_fecdoc, '%d/%m/%Y')         <alias name='campo15' />,
+            CAST(capuntes.concep AS VARCHAR(200))                   <alias name='campo16' />,
+            ''                                                      <alias name='campo17' />,
+            CAST(ROUND(capuntes.debe, 2) AS VARCHAR(15))            <alias name='campo18' />,
+            CAST(ROUND(capuntes.haber, 2) AS VARCHAR(15))           <alias name='campo19' />,
+            ''                                                      <alias name='campo20' />,
+            '1'                                                     <alias name='campo21' />,
             <whitespace/>
         </columns>
         <from table='capuntes'>
-            <join table='@tmp_tbl_cefectos' alias='tmp_cefectos'>
+            <join table='${mTmpTableCefectos}' alias='tmp_cefectos'>
                 <on>capuntes.apteid = tmp_cefectos.apteid</on>
             </join>
 
-            <join table='ctax_move_head'>
+            <join type='left' table='ctax_move_head'>
                 <on>capuntes.apteid = ctax_move_head.taxh_apteid</on>
             </join>
         </from>
-        <where>
-            ctax_move_head.taxh_fecdoc &lt;= ?
+        <where> 
+            capuntes.fecha BETWEEN ? AND ?
         </where>
     </select>
-`, mDateTimeIniPeriodoAct, mDateTimeFinPeriodoAct, mDateTimeIniPeriodoAct, mDateTimeFinPeriodoInf);
+`, mDateTimeIniPeriodoInf, mDateTimeFinPeriodoInf).toMemory();
+
+// ===============================================================
+// Arreglo de códigos de cuentas que presentan error 
+// de estructura:
+//  * campo10: tipo de comprobante de pago
+//  * campo12: número de comprobante de pago
+//  * campo15: fecha de la operación
+//  * campo16: glosa/descripción de la operación
+// ===============================================================
+var mArrayCodCtaError = [];
+
+var mIntContador = 0;
+var mNumCta = '';
+
+// ===============================================================
+// Recorrido de registros para validar la existencia de campos 
+// requeridos para la estructura de SUNAT.
+// ===============================================================
+for(let mObjRegistro of mRsPle_5_1) {
+
+    // ===============================================================
+    // Limite de 20 registros como máximo para informar 
+    // registros erróneos, limitante de cantidad de 
+    // caracteres por el frontal
+    // ===============================================================
+    if(mIntContador < 20){
+
+        // ===============================================================
+        // Construcción de la estructura del código de cuenta
+        // ===============================================================
+        mNumCta = (mObjRegistro.campo4.length > 9) ? mObjRegistro.campo4.substr(0, 9)+ '.' + mObjRegistro.campo4.substr(9) : mObjRegistro.campo4;
+
+        // ===============================================================
+        // Si algún campo requerido es null, se registra el numero 
+        // de cuenta en el arreglo definido.
+        // ===============================================================
+        if (!mObjRegistro.campo10 || !mObjRegistro.campo12 || !mObjRegistro.campo15 || !mObjRegistro.campo16) {
+            mArrayCodCtaError.push(mNumCta);
+        }
+        mIntContador++;
+    } else {
+        break;
+    }
+}
 
 // ===============================================================
 // Variables para el nombre del archivo
@@ -145,6 +179,15 @@ var mStrFileName = 'LE' + mStrRuc + mStrYear + mStrMonth + '0005010000'+ mIntInd
 // Si la condición del reporte es Fichero (F)
 // ===============================================================
 if(pStrCondicion == 'F') {
+
+    // ===============================================================
+    // Si existen números de cuenta registrados en el arreglo 
+    // se lanza una excepción que informa corregir campos 
+    // faltantes del registro
+    // ===============================================================
+    if (mArrayCodCtaError.length > 0) {
+        throw new Ax.ext.Exception(`El/los códigos de cuenta de capuntes, deben ser corregidos los tipo/número comprobante de pago, fecha de operación y descripcion: [${mArrayCodCtaError},...]`);
+    }
 
     // ===============================================================
     // Definición del blob
