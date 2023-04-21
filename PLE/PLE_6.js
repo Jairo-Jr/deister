d93488e1d4b222b2d127854fab2b86af642b3c7f
Jairo Huallpa <paul.rojas@Jairo-Huallpa.local>
2023-04-21 12:04:12 -0500

    Desarrollo de select preliminar para el PLE 6

--- PLE/PLE_6.js
+++ PLE/PLE_6.js
@@ -1,17 +1,27 @@
 var query = `
     <select>
         <columns>
-            capuntes.apteid,
-            csaldos.empcode,
-            csaldos.cuenta,
-            csaldos.codaux,
-            csaldos.ctaaux,
-            csaldos.moneda,
-            csaldos.proyec,
-            csaldos.seccio,
-            csaldos.sistem,
-            cperiodo.fecini,
-            cperiodo.fecfin
+            '20230200' <alias name='campo1' />,
+            'CUO20230200' <alias name='campo2' />,
+            'ACUO20230200' <alias name='campo3' />,
+            csaldos.cuenta <alias name='campo4' />,
+            csaldos.proyec <alias name='campo5' />,
+            csaldos.seccio <alias name='campo6' />,
+            csaldos.moneda <alias name='campo7' />,
+            '6' <alias name='campo8' />,
+            ctax_move_head.taxh_cifter <alias name='campo9' />,
+            ctax_move_head.taxh_auxchr4 <alias name='campo10' />,
+            ctax_move_head.taxh_refter <alias name='campo11' />,
+            ctax_move_head.taxh_refter <alias name='campo12' />,
+            capuntes.fecha <alias name='campo13' />,
+            cefectos.fecven <alias name='campo14' />,
+            ctax_move_head.taxh_fecdoc <alias name='campo15' />,
+            capuntes.concep <alias name='campo16' />,
+            '' <alias name='campo17' />,
+            capuntes.debe <alias name='campo18' />,
+            capuntes.haber <alias name='campo19' />,
+            '' <alias name='campo20' />,
+            '1' <alias name='campo21' />
             <!-- COUNT(*) -->
         </columns>
         <from table='csaldos'>
@@ -29,17 +39,28 @@ var query = `
                     <on>capuntes.proyec  = csaldos.proyec</on>
                     <on>capuntes.seccio  = csaldos.seccio</on>
                     <on>capuntes.sistem  = csaldos.sistem</on>
-                    <on>capuntes.fecha   BETWEEN cperiodo.fecini AND cperiodo.fecfin</on>
+                    <!-- <on>capuntes.fecha   BETWEEN cperiodo.fecini AND cperiodo.fecfin</on> -->
+                    <on>capuntes.fecha   BETWEEN '01/02/2023' AND '02/02/2023'</on>
+
+                    <join type='left' table="cefectos">
+                        <on>capuntes.apteid = cefectos.apteid</on>
+                    </join>
+
+                    <join type='left' table="ctax_move_head">
+                        <on>capuntes.apteid = ctax_move_head.taxh_apteid</on>
+                    </join>
+
                 </join>
 
             </join>
-
-            
         </from>
         <where>
             1=1
-            <!-- AND  -->
+            AND ( cperiodo.fecini = '01/02/2023' AND cperiodo.fecfin = '28/02/2023' )
         </where>
         <!-- <group>1,2,3,4,5</group> -->
     </select> 
 `;
+
+// Limitar la cantidad de datos por la fecha de capuntes
+// por el momento para tener control
