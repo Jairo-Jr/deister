d8511f98f124cfc23e579abc27f2cafc801f6577
Jairo Huallpa <paul.rojas@Jairo-Huallpa.local>
2023-04-21 08:34:58 -0500

    Carga de asientos kardex

---
+++ Kardex/carga_ene_feb.sql
@@ -0,0 +1,45 @@
+UPDATE crp_kardex SET asien_ch = '94103338' WHERE periodo = '20230100' AND tipodocto LIKE '%AJUSTE NEGATIVO FARMACIA%';
+UPDATE crp_kardex SET asien_ch = '94103339' WHERE periodo = '20230100' AND tipodocto LIKE '%AJUSTE POSITIVO FARMACIA%';
+UPDATE crp_kardex SET asien_ch = '94103339' WHERE periodo = '20230100' AND tipodocto LIKE '%FARMACIA AJUSTE NEGATIVO';
+UPDATE crp_kardex SET asien_ch = '94103339' WHERE periodo = '20230100' AND tipodocto LIKE '%FARMACIA AJUSTE NEGATIVO 2%';
+UPDATE crp_kardex SET asien_ch = '94103339' WHERE periodo = '20230100' AND tipodocto LIKE '%FARMACIA AJUSTE POSITIVO';
+UPDATE crp_kardex SET asien_ch = '94103339' WHERE periodo = '20230100' AND tipodocto LIKE '%FARMACIA AJUSTE POSITIVO 2%';
+UPDATE crp_kardex SET asien_ch = '94103339' WHERE periodo = '20230100' AND tipodocto LIKE '%REVERSA AJUSTE STOCK POSITIVO FARMACIA C%';
+
+UPDATE crp_kardex SET asien_ch = '94103338' WHERE periodo = '20230100' AND tipodocto LIKE '%G/DEVOLUCION FARMACIA (AMB-EME)%';
+UPDATE crp_kardex SET asien_ch = '94103338' WHERE periodo = '20230100' AND tipodocto LIKE '%O/DEVOLUCION FARMACIA (HOS)%';
+
+UPDATE crp_kardex SET asien_ch = '94103344' WHERE periodo = '20230100' AND tipodocto LIKE '%FARMACIA TRASPASO DE ENTRADA%';
+UPDATE crp_kardex SET asien_ch = '94103344' WHERE periodo = '20230100' AND tipodocto LIKE '%LOGÍSTICA TRASPASO DE ENTRADA%';
+UPDATE crp_kardex SET asien_ch = '94103344' WHERE periodo = '20230100' AND tipodocto LIKE '%REVERSA FARMACIA TRASPASO ENTRE ALMACENE%';
+UPDATE crp_kardex SET asien_ch = '94103344' WHERE periodo = '20230100' AND tipodocto LIKE '%REVERSA TRANSFERENCIA SALIDA CRPI%';
+UPDATE crp_kardex SET asien_ch = '94103344' WHERE periodo = '20230100' AND tipodocto LIKE '%TRANSF ENTRADA%';
+
+UPDATE crp_kardex SET asien_ch = '94103338' WHERE periodo = '20230100' AND tipodocto LIKE '%FAR. DEVOLUCION DE CONSUMOS%';
+UPDATE crp_kardex SET asien_ch = '94103338' WHERE periodo = '20230100' AND tipodocto LIKE '%FAR. ENTREGA DE CONSUMOS%';
+
+UPDATE crp_kardex SET asien_ch = '94103341' WHERE periodo = '20230100' AND tipodocto LIKE '%O/P COMPONENTES%';
+
+UPDATE crp_kardex SET asien_ch = '94103342' WHERE periodo = '20230100' AND tipodocto LIKE '%FARMACIA CONSUMO O. P. GALÉNICOS%';
+
+UPDATE crp_kardex SET asien_ch = '94103343' WHERE periodo = '20230100' AND tipodocto LIKE '%FARMACIA ORDEN DE PRODUCCIÓN MULTIDOSIS%';
+
+UPDATE crp_kardex SET asien_ch = '94103338' WHERE periodo = '20230100' AND tipodocto LIKE '%FARMACIA ORDEN DE PRODUCCIÓN MULTIDOSIS%';
+UPDATE crp_kardex SET asien_ch = '94103338' WHERE periodo = '20230100' AND tipodocto LIKE '%FAR. ENTREGA DE CONSUMOS%';
+
+UPDATE crp_kardex SET asien_ch = '94103350' WHERE periodo = '20230100' AND tipodocto LIKE '%ALBARÁN FARMACIA DEVOLUCIÓN PROVEEDOR%' AND tipologia = 'DFAR';
+UPDATE crp_kardex SET asien_ch = '94103349' WHERE periodo = '20230100' AND tipodocto LIKE '%ALBARÁN FARMACIA REINGRESO%' AND tipologia = 'IFAR';
+
+UPDATE crp_kardex SET asien_ch = '94103344' WHERE periodo = '20230100' AND tipodocto LIKE '%ALBARÁN DEVOLUCIÓN GUÍAS FLEX%';
+UPDATE crp_kardex SET asien_ch = '94103344' WHERE periodo = '20230100' AND tipodocto LIKE '%FARMACIA TRASPASO A BAJAS%';
+UPDATE crp_kardex SET asien_ch = '94103344' WHERE periodo = '20230100' AND tipodocto LIKE '%FARMACIA TRASPASO A DEVOLUCIONES%';
+UPDATE crp_kardex SET asien_ch = '94103344' WHERE periodo = '20230100' AND tipodocto LIKE '%FARMACIA TRASPASO DE SALIDA%';
+UPDATE crp_kardex SET asien_ch = '94103344' WHERE periodo = '20230100' AND tipodocto LIKE '%FARMACIA TRASPASO ENTRE ALMACENES%';
+UPDATE crp_kardex SET asien_ch = '94103344' WHERE periodo = '20230100' AND tipodocto LIKE '%TRANSF SALIDA%';
+UPDATE crp_kardex SET asien_ch = '94103344' WHERE periodo = '20230100' AND tipodocto LIKE '%TRANSFERENCIAS FARMACIA%';
+UPDATE crp_kardex SET asien_ch = '94103344' WHERE periodo = '20230100' AND tipodocto LIKE '%TRANSFERIR DE RESERVA A DISPONIBLE%';
+UPDATE crp_kardex SET asien_ch = '94103338' WHERE periodo = '20230100' AND tipodocto LIKE '%O/ENTREGA CONSIGNADOS HOS CRPI%';
+UPDATE crp_kardex SET asien_ch = '94103338' WHERE periodo = '20230100' AND tipodocto LIKE '%O/ENTREGA FARMACIA (AMB-EME)%';
+UPDATE crp_kardex SET asien_ch = '94103338' WHERE periodo = '20230100' AND tipodocto LIKE '%O/ENTREGA FARMACIA (HOS)%';
+UPDATE crp_kardex SET asien_ch = '94103338' WHERE periodo = '20230100' AND tipodocto LIKE '%O/ENTREGA FARMACIA SEDE/TERCERO%';
+UPDATE crp_kardex SET asien_ch = '94103338' WHERE periodo = '20230100' AND tipodocto LIKE '%REVERSA CONSIGNADOS HOS CRPI%';
